e4aad07f24ee9966712e71c75347414d732110cf
MattKohn <kohney@hotmail.com>
2021-12-14 15:13:09 -0700

    Complete assignment

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlantItem }) {
+  const [name, setName] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [price, setPrice] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const plantData = {
+      name: name,
+      image: imageURL,
+      price: price,
+    }
+    fetch("http://localhost:6001/plants", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(plantData),
+  })
+    .then((r) => r.json())
+    .then((newItem) => onAddPlantItem(newItem));
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="name" placeholder="Plant name" value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input 
+          type="text" 
+          name="image" placeholder="Image URL" 
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        />
+        <input 
+          type="number" 
+          name="price" 
+          step="0.01" placeholder="Price" 
+          value={price}
+          onChange={(e) => setPrice(parseFloat(e.target.value))}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+  const plantsList = plants.map(plant => (
+    <PlantCard plant={plant} />
+  ));
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantsList}
+    </ul>
   );
 }
 
